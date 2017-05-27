57f67e9f3bf8b3fd80a61e7d4106cf43d3896e83
chrisPiemonte <chris.piemonte.nj@gmail.com>
2017-05-27 17:12:40 +0200

    initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "TripWalk"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+    
---
+++ src/main/scala/Hi.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by chris on 27/05/17.
+  */
+object Hi {
+	def main(args: Array[String]) {
+		print("hello")
+	}
+}
